2017c4fa1a2c85863c5aca9eeb029acfa5fa8665
David Shafik <d.girgus@napoleongames.be>
2023-09-21 15:17:46 +0200

    test custome action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-# Container image that runs your code
-FROM alpine:3.10
+#!/bin/sh -l
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
-COPY entrypoint.sh /entrypoint.sh
-
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
-ENTRYPOINT ["/entrypoint.sh"]
+echo "Hello $1"
+time=$(date)
+echo "time=$time" >> $GITHUB_OUTPUT
