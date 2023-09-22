f4ca4b76ab3e06b25a2000995acb4d0cc16b62c3
David Shafik <d.girgus@napoleongames.be>
2023-09-22 17:14:06 +0200

    test custome action

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,11 @@ docker build -t $1/$2 $3
 echo 'Setting docker tag...'
 IMAGE_ID=$(docker images -q | head -n 1)
 echo $IMAGE_ID
-docker tag $IMAGE_ID $1/$2:$3
+docker tag $IMAGE_ID $1/$2:$6
 docker tag $IMAGE_ID $1/$2:latest
+
+echo 'Logging to docker registery'
+docker login $1 -u $4 -p $5
 
 echo 'Pushing docker image...'
 docker push $1/$2:$6
