f7fb250f2f615fb30a8d2489db1598190cc725ac
David Shafik <d.girgus@napoleongames.be>
2023-09-22 16:57:16 +0200

    test custome action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
-
 echo 'Docker building...'
 docker build -t $1/$2 $3
 
 echo 'Setting docker tag...'
 IMAGE_ID=$(docker images -q | head -n 1)
-docker tag $IMAGE_ID $1/$2:$6
+echo $IMAGE_ID
+docker tag $IMAGE_ID $1/$2:$3
 docker tag $IMAGE_ID $1/$2:latest
 
 echo 'Pushing docker image...'
