abd32fe9ab204712a5baddf1b931583fdf0aad48
David Shafik <d.girgus@napoleongames.be>
2023-09-22 16:43:17 +0200

    test custome action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
-docker build .
-echo "Hello $1 i greet you because $2 "
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+
+echo 'Docker building...'
+docker build -t $1/$2 $3
+
+echo 'Setting docker tag...'
+IMAGE_ID=$(docker images -q | head -n 1)
+docker tag $IMAGE_ID $1/$2:$6
+docker tag $IMAGE_ID $1/$2:latest
+
+echo 'Pushing docker image...'
+docker push $1/$2:$6
+docker push $1/$2:latest;
+
+echo "image-name=$1/$2:$6" >> $GITHUB_OUTPUT
